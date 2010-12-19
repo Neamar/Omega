f152e720390db704a005dd8f568d559dc00cefbf
Matthieu Bacconnier <matthieu@bacconnier.fr>
2010-12-19 12:57:41 +0100

    Création du contrôleur d'exercice

---
+++ lib/AbstractController/ExerciceAbstractController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * exerciceAbstractController.php - 26 oct. 2010
+ * 
+ * Actions de base communes aux contrôleurs d'exercices : récupération par l'ID, vérification des droits...
+ * 
+ * PHP Version 5
+ * 
+ * @category  Controller
+ * @package   Root
+ * @author    Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @copyright 2010 Matthieu Bacconnier
+ * @license   Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link      http://eDevoir.com
+ */
+
+/**
+ * Couche d'abstraction pour les exercices
+ * 
+ * @category Controller
+ * @package  Root
+ * @author   Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @license  Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link     http://eDevoir.com
+ *
+ */
+abstract class ExerciceAbstractController extends AbstractController
+{
+
+}
