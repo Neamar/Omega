eed3d20ff1fb5fe741074a1305ad30fe73a78211
Anthony <anthony.tschirhard@gmai.com>
2011-01-05 13:45:36 +0100

    Documentation correcteur

---
+++ application/documentation/correcteur/correcteurController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * correcteurController.php - 30 dec. 2010
+ * 
+ * Documentation correcteur.
+ * 
+ * PHP Version 5
+ * 
+ * @category  Controller
+ * @package   Root
+ * @author    Matthieu Bacconnier <matthieu@bacconnier.fr> and associates ! :D <- non je déconne !
+ * @copyright 2010 Matthieu Bacconnier
+ * @license   Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link      http://edevoir.com
+ */
+
+//Charger la base.
+include APPLICATION_PATH . '/documentation/index/indexController';
+
+/**
+ * Contrôleur de documentation générique.
+ * 
+ * @category Controller
+ * @package  Root
+ * @author   Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @license  Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link     http://edevoir.com
+ *
+ */
+class Documentation_CorrecteurController extends Documentation_IndexController
+{
+}
