5e41dc9649747680446569a43f67f15d4d1e8e34
Matthieu Bacconnier <matthieu@bacconnier.fr>
2011-01-06 20:02:18 +0100

    Choix des matières de compétence (pas d'enregistrement)

--- public/js/correcteur/index/options_matieres.js
+++ public/js/correcteur/index/options_matieres.js
@@ -1,43 +1,39 @@
 $(function() {
-	$( ".slider" ).each(function(){
+	$(".matiere-box input[type=checkbox]").change(function(){
+		if(this.checked)
+		{
+			$(this).closest('div').find('div').show('slow');
+		}
+		else
+		{
+			$(this).closest('div').find('div').hide('slow');
+		}
+	});
+	
+	$(".matiere-box div").hide();
+});
+
+$(function() {
+	/**
+	 * Effectue la transition entre les coordonnées "slider" et les coordonnées "combobox".
+	 * @param val la valeur à transformer
+	 * @returns int la valeur transformée
+	 */
+	function translate(val)
+	{
+		return ClasseMax - val + ClasseMin;
+	}
+	
+	$(".slider").each(function(){
 		var slider = $(this);
 		
 		//Récupérer les select associés
 		slider.data('start', $(slider.data('start')));
 		slider.data('end', $(slider.data('end')));
 		
-		var disableFunction = function()
-		{
-			values = slider.slider("option", "values");
-			values[0] = translate(values[0]);
-			values[1] = translate(values[1]);
-			
-			slider.data('start').find('option').each(function(){
-				if(parseInt(this.value) < values[1])
-					this.disabled = "disabled";
-				else
-					this.removeAttribute('disabled');
-			});
-			
-			slider.data('end').find('option').each(function(){
-				if(parseInt(this.value) >= values[0])
-					this.disabled = "disabled";
-				else
-					this.removeAttribute('disabled');
-			})
-		};
 		
-		var changeFunction = function(){
-			NValues = [ translate(slider.data('start').val()),  translate(slider.data('end').val())];
-			if(slider.slider("option","values") != NValues)
-			{
-				slider.slider("option", "values", [ translate(slider.data('start').val()),  translate(slider.data('end').val())]);
-				disableFunction();
-			}
-		};
-		
-		slider.data('start').change(changeFunction);
-		slider.data('end').change(changeFunction);
+		slider.data('start').attr('disabled', 'readonly');
+		slider.data('end').attr('disabled', 'readonly');
 		
 		slider.slider({
 			range: true,
@@ -50,18 +46,7 @@ $(function() {
 			slide: function( event, ui ) {
 				$(this).data('start').val(translate(ui.values[0]));
 				$(this).data('end').val(translate(ui.values[1]));
-				disableFunction();
 			}
 		});
 	});
 });
-
-/**
- * Effectue la transition entre les coordonnées "slider" et les coordonnées "combobox".
- * @param val la valeur à transformer
- * @returns int la valeur transformée
- */
-function translate(val)
-{
-	return ClasseMax - val + ClasseMin;
-}
