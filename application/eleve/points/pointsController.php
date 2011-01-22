6ec4e5e50518653add78ff1272e04b1dae59e305
Matthieu Bacconnier <matthieu@bacconnier.fr>
2011-01-22 17:51:07 +0100

    Création des contrôleurs de points

---
+++ application/eleve/points/pointsController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * pointsController.php - 22 janv. 2011
+ * 
+ * Gestion des points du compte de l'élève
+ * 
+ * PHP Version 5
+ * 
+ * @category  Controller
+ * @package   Root
+ * @author    Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @copyright 2010 Matthieu Bacconnier
+ * @license   Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link      http://edevoir.com
+ * @param string
+ * @param string
+ */
+ 
+/**
+ * Actions sur les points
+ *
+ * @category Controller
+ * @package  Root
+ * @author   Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @license  Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link     http://edevoir.com
+ */
+class Eleve_PointsController extends PointsAbstractController
+{
+}
---
+++ lib/AbstractController/PointsAbstractController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PointsAbstractController.php - 22 janv. 2011
+ * 
+ * Contrôleur abstrait pour la gestion des points d'un compte.
+ * 
+ * PHP Version 5
+ * 
+ * @category  Controller
+ * @package   Root
+ * @author    Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @copyright 2010 Matthieu Bacconnier
+ * @license   Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link      http://edevoir.com
+ */
+ 
+/**
+ * Gestion des points
+ *
+ * @category Controller
+ * @package  Root
+ * @author   Matthieu Bacconnier <matthieu@bacconnier.fr>
+ * @license  Copyright http://fr.wikipedia.org/wiki/Copyright
+ * @link     http://edevoir.com
+ */
+abstract class PointsAbstractController extends AbstractController
+{
+
+}
