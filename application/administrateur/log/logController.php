50c2a300a6b4b507fab5a7aa9d9c3ce9c613d5eb
Matthieu Bacconnier <matthieu@bacconnier.fr>
2011-02-16 15:12:57 +0100

    Logs : exercices

--- application/administrateur/log/logController.php
+++ application/administrateur/log/logController.php
@@ -33,23 +33,39 @@ class Administrateur_LogController extends AbstractController
 	public function exerciceAction()
 	{
 		$this->View->setTitle(
-			"Exercices",
-			"Cette page permet de consulter en quasi temps réel ."
+			"Informations Exercices",
+			"Cette page permet de consulter en quasi temps réel les informations sur les exercices."
+		);
+	}
+	
+	/**
+	 * Derniers exercices
+	 */
+	public function _exerciceAction()
+	{
+		$this->ajax('SELECT
+				DATE_FORMAT(Exercices.Creation,"%d/%c/%y à %Hh"),
+				Exercices.Titre,
+				DATE_FORMAT(Exercices.Expiration,"%d/%c/%y à %Hh"),
+				CONCAT("<a href=/administrateur/exercice/index/", Exercices.Hash,">Consulter</a>")
+			FROM Exercices',
+			'Exercices.Creation DESC'
 		);
 	}
 	
 	/**
 	 * Dernières actions sur les exercices
 	 */
-	public function _exerciceAction()
+	public function _exercice_logAction()
 	{
 		$this->ajax('SELECT
-			DATE_FORMAT(Exercices_Logs.Date,"%d/%c/%y à %Hh"),
-			Exercices.Titre,
-			Exercices_Logs.Action, 
-			CONCAT("<a href=/administrateur/exercice/index/", Exercices.Hash,">Consulter</a>")
-		FROM Exercices_Logs
-		JOIN Exercices ON (Exercices_Logs.Exercice = Exercices.ID)');
+				DATE_FORMAT(Exercices_Logs.Date,"%d/%c/%y à %Hh"),
+				Exercices.Titre,
+				Exercices_Logs.Action, 
+				CONCAT("<a href=/administrateur/exercice/index/", Exercices.Hash,">Consulter</a>")
+			FROM Exercices_Logs
+			JOIN Exercices ON (Exercices_Logs.Exercice = Exercices.ID)'
+		);
 	}
 	
 	public function eleveAction()
